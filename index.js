66ae41c6f169031b474f144dfe3152069d7c1cae
Lalith D <dlalith422@gmail.com>
2024-01-26 17:42:56 +0530

    CSS has been added

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-
+// let input = document.querySelector('input');
+// input.addEventListener('keyup' ,(e)=>
+// {
+//     if(e.keycode === 13)
+//     {
+//         console.log(e.target.value);
+//     }
+// })
 
 async function fetchdata() {
   try {
-    const pokemonname= document.getElementById("pokemonname").value.toLowerCase();
+    const pokemonname = document
+      .getElementById("pokemonname")
+      .value.toLowerCase();
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonname}`
+    );
 
     if (!response.ok) {
       throw new Error("couldnt fetch data");
     }
     const data = await response.json();
-    // console.log(data);
+    console.log(data);
     const pokemonSprite = data.sprites.front_default;
     const imgEle = document.getElementById("pokemonSprite");
     imgEle.src = pokemonSprite;
     imgEle.style.display = "block";
-     console.log(pokemonname);
+
+   // Assuming your object is stored in a variable named 'pokemonData'
+   const pokemonData = await response.jason();
+const pokemonName = pokemonData.species.name;
+console.log(pokemonName); // Output: pikachu
+
   } 
   catch (error) {
     console.error(error);
   }
 }
-
-fetchdata();
