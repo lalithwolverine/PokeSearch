c5888268fe0def081aa40a9491343e2786899af2
Lalith D <dlalith422@gmail.com>
2024-01-27 20:58:57 +0530

    Alert and Enter key added

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-// let input = document.querySelector('input');
-// input.addEventListener('keyup' ,(e)=>
-// {
-//     if(e.keycode === 13)
-//     {
-//         console.log(e.target.value);
-//     }
-// })
-
 async function fetchdata() {
   try {
-    const pokemonname = document
-      .getElementById("pokemonname")
-      .value.toLowerCase();
+    const pokemonname = document.getElementById("pokemonname").value.toLowerCase();
 
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemonname}`
     );
 
     if (!response.ok) {
-      throw new Error("couldnt fetch data");
+      throw new Error("Couldn't fetch data");
     }
+
     const data = await response.json();
     console.log(data);
+
     const pokemonSprite = data.sprites.front_default;
     const imgEle = document.getElementById("pokemonSprite");
     imgEle.src = pokemonSprite;
     imgEle.style.display = "block";
 
-   // Assuming your object is stored in a variable named 'pokemonData'
-   const pokemonData = await response.jason();
-const pokemonName = pokemonData.species.name;
-console.log(pokemonName); // Output: pikachu
-
-  } 
-  catch (error) {
+    
+  
+  } catch (error) {
     console.error(error);
   }
 }
+
+//ENTER KEY TO SEARCH
+const search = document.getElementById("pokemonname");
+
+function showAlert() {
+  const inputvalue=search.value.trim();
+  if(inputvalue==="")
+  {
+    alert("Enter a Poke name");
+  }
+  else{
+  fetchdata();
+  }
+}
+
+search.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    showAlert();
+  }
+});
